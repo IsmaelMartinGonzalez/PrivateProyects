4fbc1c7dc840ec8fb10f762a9a6b0f81eade52d5
IsmaelMartinGonzalez <fmartin@nigul.cide.es>
2021-01-11 12:33:32 +0100

    Creamos entorno de trabajo Pac-Man

---
+++ Pac-Man/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Project name: PrivateProyects/PACKAGE_NAME
+ * Filename:
+ * Created:  11/01/2021 / 12:32
+ * Description:
+ * Revision:
+ *
+ * @Author: Ismael - fmartin@nigul.cide.es
+ * @Version:
+ */
+public class Main {
+    public static void main(String[] args) {
+        /**Aqui va el play*/
+    }
+}
